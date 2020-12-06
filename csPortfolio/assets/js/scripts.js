3a1a1580a9ffdb9cb43e59ab83b2b4978b8b0a78
apriljames <jamesap@oregonstate.edu>
2020-12-06 13:11:47 -0800

    more styling, customized icon/cursor.

--- csPortfolio/assets/js/scripts.js
+++ csPortfolio/assets/js/scripts.js
@@ -1,1 +1,72 @@
 $('.top-content').backstretch("assets/img/backgrounds/1.jpg");
+
+
+
+
+/********************************* */
+// add scroll animation and make navbar links work
+/********************************* */
+function scroll_to(clicked_link, nav_height) {
+    var element_class = clicked_link.attr('href').replace('#', '.');
+    var scroll_to = 0;
+    if(element_class != '.top-content') {
+        element_class += '-container';
+        scroll_to = $(element_class).offset().top - nav_height;
+    }
+    if($(window).scrollTop() != scroll_to) {
+        $('html, body').stop().animate({scrollTop: scroll_to}, 1000);
+    }
+}
+
+/********************************* */
+// add typewriter animation to header description
+/********************************* */
+var occupation = document.getElementById('typed-occupation');
+
+var typedOccupation = new Typewriter(occupation, {
+    loop: false,
+    delay: 40
+});
+
+typedOccupation.typeString('senior @ oregon state, studying computer science. ')
+    .pauseFor(300)
+    .deleteChars(2)
+    .pauseFor(100)
+    .typeString(' & studio art')
+    .pauseFor(2500)
+    .start();
+
+
+jQuery(document).ready(function() {
+ 
+    // ...
+ 
+    // toggle "navbar-no-bg" class
+    $('.top-content .text').waypoint(function() {
+        $('nav').toggleClass('navbar-no-bg');
+    });
+    
+    // Navigation
+   $('a.scroll-link').on('click', function(e) {
+        e.preventDefault();
+        scroll_to($(this), $('nav').outerHeight());
+    });
+    
+
+    // back-to-top button functionality
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            $('.back-to-top').fadeIn();
+        } else {
+            $('.back-to-top').fadeOut();
+        }
+    });
+    // scroll body to 0px on click
+    $('.back-to-top').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 400);
+        return false;
+    });
+ 
+});
--- csPortfolio/codefolio.php
+++ csPortfolio/codefolio.php
@@ -7,7 +7,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
  
-        <title>Bootstrap 4 One Page Website Tutorial</title>
+        <title>april's portfolio site</title>
  
         <!-- CSS -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,600">
@@ -19,7 +19,7 @@
         <!-- <link rel="stylesheet" href="assets/css/media-queries.css"> -->
  
         <!-- Favicon and touch icons -->
-        <link rel="shortcut icon" href="assets/ico/favicon.png">
+        <link rel="shortcut icon" href="assets/imgs/icons/bun.png">
         <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/apple-touch-icon-144-precomposed.png">
         <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/ico/apple-touch-icon-114-precomposed.png">
         <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/apple-touch-icon-72-precomposed.png">
@@ -40,44 +40,41 @@
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav ml-auto">
                         <li class="nav-item">
-                            <a class="nav-link scroll-link" href="#top-content">Top</a>
+                            <a class="nav-link scroll-link" href="#top-content">top</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link scroll-link" href="#services">Services</a>
+                            <a class="nav-link scroll-link" href="#about-me">about me</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link scroll-link" href="#about-us">About</a>
+                            <a class="nav-link scroll-link" href="#portfolio">work</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link scroll-link" href="#portfolio">Work</a>
+                            <a class="nav-link scroll-link" href="#experience">resume</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link scroll-link" href="#testimonials">Clients</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link scroll-link" href="#blog">Blog</a>
+                            <a class="nav-link scroll-link" href="#contact">contact</a>
                         </li>
                     </ul>
                 </div>
             </div>
         </nav>
 
+        <!-- sticky back-to-top button -->
+        <a id="back-to-top" href="#" class="btn circle-btn back-to-top" role="button"><i class="fas fa-chevron-up"></i></a>
+
         <!-- Top content -->
-        <div class="top-content">
+        <div class="section-container top-content">
             <div class="container">
         
                 <div class="row">
-                    <div class="col-md-8 offset-md-2 text">
-                        <h1 class="wow fadeInLeftBig">Bootstrap 4 Website Tutorial</h1>
-                        <div class="description wow fadeInLeftBig">
-                            <p>
-                                This is a free responsive one page template made with the Bootstrap framework. 
-                                Check it out now. Download it, customize and use it as you like!
-                            </p>
+                    <div class="col-md-8 section-description offset-md-2 text">
+                        <h1 class="wow fadeInLeftBig">april.james</h1>
+                        <div id="typed-occupation" class="description">
+                            <!-- space reserved for dynamic typing: "computer science student, senior @ oregon state university" -->
                         </div>
                         <div class="top-big-link wow fadeInUp">
-                            <a class="btn btn-primary btn-link-1" href="https://azmind.com">Download it</a>
-                            <a class="btn btn-primary btn-link-2 scroll-link" href="#services">Learn more</a>
+                            <a class="btn circle-btn scroll-link" href="#about-me"><i class="fas fa-arrow-down"></i></a>
+                            
                         </div>
                     </div>
                 </div>
@@ -86,55 +83,40 @@
         </div>
 
 
-        <!-- Services -->
-        <div class="services-container section-container">
+        <!-- about-me -->
+        <div class="about-me-container section-container">
             <div class="container">
                 <div class="row">
-                    <div class="col services section-description wow fadeIn">
-                        <h2>Our Services</h2>
+                    <div class="col about-me section-description wow fadeIn">
+                        <h2>about me</h2>
                         <div class="divider-1 wow fadeInUp"><span></span></div>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col-md-4 services-box wow fadeInUp">
-                        <div class="row">
-                            <div class="col-md-4">
-                                <div class="services-box-icon">
-                                    <i class="fas fa-magic"></i>
-                                </div>
-                            </div>
-                            <div class="col-md-8">
-                                <h3>Web Design</h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-4 services-box wow fadeInDown">
-                        <div class="row">
-                            <div class="col-md-4">
-                                <div class="services-box-icon">
-                                    <i class="fas fa-cog"></i>
-                                </div>
-                            </div>
-                            <div class="col-md-8">
-                                <h3>UI / UX</h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-4 services-box wow fadeInUp">
-                        <div class="row">
-                            <div class="col-md-4">
-                                <div class="services-box-icon">
-                                    <i class="fab fa-google"></i>
-                                </div>
-                            </div>
-                            <div class="col-md-8">
-                                <h3>Marketing</h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et.</p>
-                            </div>
-                        </div>
-                    </div>
+                <div class="row justify-content-center">
+                    <div class="col-md-10 about-me-box wow fadeInUp">
+                        <p>
+                            Hello! I'm April, a senior at Oregon State majoring in computer science with a focus in 
+                            graphics & web development. I'm minoring studio arts, with special interest in graphic design
+                            and printmaking. My projected graduation date is spring 2022. 
+                        </p>
+                        <p>
+                            I'm a member of Adaptive Technology Engineering Network (<a class="txt-link" href="https://groups.engr.oregonstate.edu/aten/" target="_blank">ATEN</a>) club, 
+                            and the production officer for the Graphic Design club, where I look forward to managing their 
+                            <a class="txt-link" href="https://oregonstate-gd.com/" target="_blank">website</a> soon. At OSU, I've been a technical assistant 
+                            for an Electrical Fundamentals course, and worked as a designer for the Asian & Pacific Cultural Center 
+                            (some work showcased <a class="txt-link" href="https://aprilscanvas.com/graphicDesign/index.php" target="_blank">here</a>). 
+                        </p>
+                        <p>
+                            Over spring-summer 2020, I had the opportunity to work as a software engineering intern for Intel's 
+                            Wireless Platform Modules team, where I helped modify automation scripts in Python and SKILL, and 
+                            developed a working prototype for a full-stack web app using HTML/CSS/JQuery/ASP.NET/SQL Server to help 
+                            PMs analyze build plan data. 
+                        </p>
+                        <p>
+                            With spare time, I like to bike, rock climb, dance cuban salsa, and make food. I doodle a bit, which 
+                            you can take a peek at on my <a class="txt-link" href="https://aprilscanvas.com/" target="_blank">portfolio</a>.  
+                        </p>                         
+                    </div>  
                 </div>
             </div>
         </div>
@@ -144,62 +126,83 @@
             <div class="container">
                 <div class="row">
                     <div class="col portfolio section-description wow fadeIn">
-                        <h2>Our Portfolio</h2>
+                        <h2>projects</h2>
                         <div class="divider-1 wow fadeInUp"><span></span></div>
-                        <p>We've completed 537 projects since we started back in 2010. Check them out!</p>
+                        <p>here's the place where I link some projects.</p>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-md-4 portfolio-box wow fadeInUp">
-                        <div class="portfolio-box-image">
+                        <!-- <div class="portfolio-box-image">
                             <img src="assets/img/portfolio/1.jpg" alt="" data-at2x="assets/img/portfolio/1.jpg">
-                        </div>
-                        <h3><a href="#">Acme branding</a> <i class="fas fa-angle-right"></i></h3>
+                        </div> -->
+                        <h3><a href="#">thing</a> <i class="fas fa-angle-right"></i></h3>
                         <div class="portfolio-box-date"><i class="far fa-calendar"></i> March 2018</div>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et.
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et.
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et.
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et.
+                        </p>
                     </div>
                     <div class="col-md-4 portfolio-box wow fadeInDown">
-                        <div class="portfolio-box-image">
+                        <!-- <div class="portfolio-box-image">
                             <img src="assets/img/portfolio/2.jpg" alt="" data-at2x="assets/img/portfolio/2.jpg">
-                        </div>
-                        <h3><a href="#">WordPress design</a> <i class="fas fa-angle-right"></i></h3>
+                        </div> -->
+                        <h3><a href="#">another thing</a> <i class="fas fa-angle-right"></i></h3>
                         <div class="portfolio-box-date"><i class="far fa-calendar"></i> February 2018</div>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et.</p>
                     </div>
                     <div class="col-md-4 portfolio-box wow fadeInUp">
-                        <div class="portfolio-box-image">
+                        <!-- <div class="portfolio-box-image">
                             <img src="assets/img/portfolio/3.jpg" alt="" data-at2x="assets/img/portfolio/3.jpg">
-                        </div>
-                        <h3><a href="#">Created 150 flyers</a> <i class="fas fa-angle-right"></i></h3>
+                        </div> -->
+                        <h3><a href="#">frog</a> <i class="fas fa-angle-right"></i></h3>
                         <div class="portfolio-box-date"><i class="far fa-calendar"></i> January 2018</div>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et.</p>
                     </div>
                 </div>
-                <div class="row">
+                <!-- <div class="row">
                     <div class="col section-bottom-button wow fadeInUp">
                         <a class="btn btn-primary btn-link-3" href="#">Load more</a>
+                    </div>
+                </div> -->
+            </div>
+        </div>
+
+        <!-- Experience -->
+        <div class="experience-container section-container">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-4 section-description text">
+                        <h1 class="wow fadeInLeftBig">experience</h1>
+                        <div class="description wow fadeInLeftBig">
+                            <p>
+                                please view my resume here:
+                            </p>
+                        </div>
+                        <div class="top-big-link wow fadeInUp">
+                            <a class="btn btn-primary btn-link-1" href="https://azmind.com">view my resume</a>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
 
-        
-
 
         <!-- end of the page's code -->
  
         <!-- Javascript -->
-        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-        <script src="https://code.jquery.com/jquery-migrate-3.3.2.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script>        <script src="https://code.jquery.com/jquery-migrate-3.3.2.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-backstretch/2.1.18/jquery.backstretch.min.js" integrity="sha512-bXc1hnpHIf7iKIkKlTX4x0A0zwTiD/FjGTy7rxUERPZIkHgznXrN/2qipZuKp/M3MIcVIdjF4siFugoIc2fL0A==" crossorigin="anonymous"></script>
         <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-backstretch/2.1.18/jquery.backstretch.js" integrity="sha512-hnO4ypSVsbX/EBMaO/auYhsgRSSWftlgxCsJjqIHWUIGeAQWefevOlG8OrnopZ7eDER+xn/rOtSXqSWHkvxsOA==" crossorigin="anonymous"></script> -->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js" integrity="sha512-Eak/29OTpb36LLo2r47IpVzPBLXnAMPAVypbSZiZ4Qkf8p/7S/XRG5xp7OKWPPYfJT6metI+IORkR5G8F900+g==" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/retina.js/2.1.3/retina.min.js" integrity="sha512-9RcSZpXlWiAq12BM82QI0qRtusQJonuUZ/h/bZl+Cd6O4Pw7UnGV6gnCcS5wV03/zvLwCeL3enaUa0RPy+VM2w==" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/noframework.waypoints.min.js" integrity="sha512-fHXRw0CXruAoINU11+hgqYvY/PcsOWzmj0QmcSOtjlJcqITbPyypc8cYpidjPurWpCnlB8VKfRwx6PIpASCUkQ==" crossorigin="anonymous"></script>
-
-        <!-- <script src="assets/js/jquery.backstretch.min.js"></script> -->
+        <!-- execute functions when you scroll to them -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js" integrity="sha512-CEiA+78TpP9KAIPzqBvxUv8hy41jyI3f2uHi7DGp/Y/Ka973qgSdybNegWFciqh6GrN2UePx2KkflnQUbUhNIA==" crossorigin="anonymous"></script>        <!-- <script src="assets/js/jquery.backstretch.min.js"></script> -->
+        <!-- typing effect -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/TypewriterJS/2.13.1/core.min.js" integrity="sha512-yfhC0kG8fvDDLG3xpuZ4fZ2zCoZKHzkoO/mCFdDiUzwKktWnYkXZwNjW1qyoMwnf1uRi8LelY5wDNIA30Xz7Dw==" crossorigin="anonymous"></script>
         <!-- <script src="assets/js/wow.min.js"></script> -->
         <!-- <script src="assets/js/retina-1.1.0.min.js"></script> -->
         <!-- <script src="assets/js/waypoints.min.js"></script> -->
